22f81f525b97ac79c00cdf1af7c4f15f0cdaca3c
Andrey Sinitsyn <andrey.sin98@gmail.com>
2017-07-05 01:24:51 +0300

    #1: Updates User model to enable possibility for OAuth integration

--- packages/backend/src/schema/User.js
+++ packages/backend/src/schema/User.js
@@ -1,7 +1,31 @@
-import mongoose, { Schema } from 'mongoose';
-
-const { Types } = Schema;
+import { Schema } from 'mongoose';
 
 const userSchema = new Schema({
   username: { type: String, required: true, index: true, unique: true },
+  reddit: {
+    uid: { type: String, index: true },
+    username: { type: String },
+    accessToken: String,
+    accessTokenExpiration: Date,
+  },
+  slack: {
+    uid: { type: String, index: true },
+    username: { type: String },
+    accessToken: String,
+    accessTokenExpiration: Date,
+  },
+  github: {
+    uid: { type: String, index: true },
+    username: { type: String },
+    accessToken: String,
+    accessTokenExpiration: Date,
+  },
+  gitlab: {
+    uid: { type: String, index: true, unique: true },
+    username: { type: String },
+    accessToken: String,
+    accessTokenExpiration: Date,
+  },
 });
+
+module.exports = [userSchema];
