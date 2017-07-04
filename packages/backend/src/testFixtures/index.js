78982cccc2dddf9cc904f71b0dc1a5d3c5c7a3eb
MatejMecka <matej.plavevski+github@gmail.com>
2017-07-04 19:56:44 +0200

    #9: Adds Express session middleware, several fixes to linting

--- packages/backend/src/testFixtures/index.js
+++ packages/backend/src/testFixtures/index.js
@@ -1,7 +1,9 @@
 import request from 'request-promise';
+
 export const SERVER_URL = 'http://localhost:3000';
 export const API_URL = `${SERVER_URL}/api/`;
 
+/* eslint-disable */
 export async function clearDB() {
   
 }
@@ -17,6 +19,7 @@ export async function loginAsDemoUser() {
 export async function startAuthenticatedRequest() {
 
 }
+/* eslint-enable */
 
 export function initRequestLibrary() {
   request.defaults({ jar: true, resolveWithFullResponse: true });
