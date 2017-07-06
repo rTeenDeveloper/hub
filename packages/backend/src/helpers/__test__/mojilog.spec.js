36599cae7120dd118c1453342848a1ef889b5304
Andrey Sinitsyn <andrey.sin98@gmail.com>
2017-07-06 16:03:37 +0300

    Fixes mojilog and adds unit tests for it

---
+++ packages/backend/src/helpers/__test__/mojilog.spec.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-undef */
+import chalk from 'chalk';
+import logger from '../mojilog';
+
+describe('mojilog', () => {
+  beforeEach(() => {
+    global.console = {
+      warn: jest.fn(),
+      log: jest.fn(),
+      error: jest.fn(),
+    };
+  });
+
+  describe('.inProd()', () => {
+    test('should log to the console', () => {
+      logger.inProd('test');
+      expect(global.console.log).toBeCalledWith(
+        `${chalk.bold.bgGreen.inverse(' INFO ')} ${chalk.green('test')}`
+      );
+    });
+  });
+
+  describe('.inTest()', () => {
+    test('should log to the console on environments different from production', () => {
+      logger.inTest('test');
+      expect(global.console.log).toBeCalledWith(
+        `${chalk.bold.bgMagenta.inverse(' VERB ')} ${chalk.magenta('test')}`
+      );
+    });
+
+    test('should not log to the console on production', () => {
+      process.env.NODE_ENV = 'production';
+      logger.inTest('test');
+      expect(global.console.log).not.toBeCalled();
+    });
+  });
+
+  describe('.toInvestigate()', () => {
+    test('should log message to the console', () => {
+      logger.toInvestigate('test');
+      expect(global.console.warn).toBeCalledWith(
+        `${chalk.bold.bgYellow.inverse(' WARN ')} ${chalk.yellow('test')}`
+      );
+    });
+    test('should log error to the console', () => {
+      const err = new Error('');
+      logger.toInvestigate(err);
+      expect(global.console.warn).toBeCalledWith(
+        logger
+          .formatError(err)
+          .map(
+            item =>
+              `${chalk.bold.bgYellow.inverse(' WARN ')} ${chalk.yellow(item)}`
+          )
+          .join('\n')
+      );
+    });
+  });
+
+  describe('.ACHTUNG_ALL_BROKEN()', () => {
+    test('should log message to the console', () => {
+      logger.ACHTUNG_ALL_BROKEN('test');
+      expect(global.console.error).toBeCalledWith(
+        `${chalk.bold.bgRedBright.inverse(' ERR  ')} ${chalk.redBright('test')}`
+      );
+    });
+    test('should log error to the console', () => {
+      const err = new Error('');
+      logger.ACHTUNG_ALL_BROKEN(err);
+      expect(global.console.error).toBeCalledWith(
+        logger
+          .formatError(err)
+          .map(
+            item =>
+              `${chalk.bold.bgRedBright.inverse(' ERR  ')} ${chalk.redBright(
+                item
+              )}`
+          )
+          .join('\n')
+      );
+    });
+  });
+
+  describe('.formatError()', () => {
+    test('it should transform exception to array of strings', () => {
+      const err = new Error();
+      expect(logger.formatError(err)).toEqual([
+        '--- STACKTRACE START ---',
+        ...err.stack.split('\n'),
+        '--- STACKTRACE END ---',
+      ]);
+    });
+  });
+});
--- packages/backend/src/helpers/mojilog.js
+++ packages/backend/src/helpers/mojilog.js
@@ -2,30 +2,38 @@ import chalk from 'chalk';
 
 export default class Logger {
   static inProd(msg) {
-    console.log(`${chalk.bold.green.inverse(' INFO ')} ${chalk.green(msg)}`);
+    console.log(Logger.formatString(' INFO ', msg, 'green'));
+  }
+
+  static formatString(tag, string, color) {
+    return `${chalk.bold[
+      `bg${color.substr(0, 1).toUpperCase()}${color.substr(1)}`
+    ].inverse(tag)} ${chalk[color](string)}`;
   }
 
   static inTest(msg) {
     if (process.env.NODE_ENV !== 'production')
-      console.log(
-        `${chalk.bold.magenta.inverse(' VERB ')} ${chalk.magenta(msg)}`
-      );
+      console.log(Logger.formatString(' VERB ', msg, 'magenta'));
   }
 
   static toInvestigate(msg) {
     if (msg instanceof Error)
-      return Logger.formatError(msg).forEach(::Logger.toInvestigate);
-    return console.warn(
-      `${chalk.bold.yellow.inverse(' WARN ')} ${chalk.yellow(msg)}`
-    );
+      return console.warn(
+        Logger.formatError(msg)
+          .map(item => `${Logger.formatString(' WARN ', item, 'yellow')}`)
+          .join('\n')
+      );
+    return console.warn(Logger.formatString(' WARN ', msg, 'yellow'));
   }
 
   static ACHTUNG_ALL_BROKEN(err) {
     if (err instanceof Error)
-      return Logger.formatError(err).forEach(::Logger.ACHTUNG_ALL_BROKEN);
-    return console.error(
-      `${chalk.bold.redBright.inverse(' ERR  ')} ${chalk.redBright(err)}`
-    );
+      return console.error(
+        Logger.formatError(err)
+          .map(item => `${Logger.formatString(' ERR  ', item, 'redBright')}`)
+          .join('\n')
+      );
+    return console.error(Logger.formatString(' ERR  ', err, 'redBright'));
   }
 
   static formatError(err) {
