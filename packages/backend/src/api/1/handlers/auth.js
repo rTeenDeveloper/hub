ec7187e535f450124f89a0ae267eabed8da954bc
Rishav Kundu <rishav.kundu98@gmail.com>
2017-07-06 19:58:01 +0530

    Implement passport.js reddit auth
    Functions to
    
    1. authenticate Reddit IDs against the database
    2. Serialize the User object into a session
    3. Handle the case of new signups
    
    have to be implemented.

--- packages/backend/src/api/1/handlers/auth.js
+++ packages/backend/src/api/1/handlers/auth.js
@@ -1,3 +1,29 @@
+import crypto from 'crypto';
 import express from 'express';
+import passport from 'passport';
 
-module.exports = express.Router();
+var router = express.Router();
+
+router.get('/reddit/callback', function(req, res, next) {
+    // Check for origin via state token
+    if (req.query.state == req.session.state) {
+        passport.authenticate('reddit', {
+            successRedirect: '/auth_status',
+            failureRedirect: '/login_failure',
+        })(req, res, next);
+    } else {
+        next(new Error(403));
+    }
+});
+
+router.get('/reddit', function(req, res, next) {
+    if (!req.session.state) {
+      req.session.state = crypto.randomBytes(32).toString('hex');
+    }
+    passport.authenticate('reddit', {
+        state: req.session.state,
+        duration: 'permanent',
+    })(req, res, next)
+});
+
+module.exports = router;
--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -11,8 +11,19 @@ import passport from 'passport';
 import packageFile from '../package.json';
 import logger from './helpers/mojilog';
 import createApiVersioningRouter from './helpers/createApiVersioningRouter';
+import setupPassportStrategies from './helpers/setupPassportStrategies';
 
 const routesPath = path.join(__dirname, 'routes');
+
+passport.serializeUser(function(user, done) {
+  // TODO: serialize
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  // TODO: deserialize
+  done(null, obj);
+});
 
 // eslint-disable-next-line
 export function createServer(bind) {
@@ -35,11 +46,12 @@ export function createServer(bind) {
     app.use(passport.initialize());
     app.use(passport.session());
     app.use(cors({ origin: true, credentials: true }));
+    setupPassportStrategies(passport);
     if (environment === 'development') app.use(requestLogger('dev'));
     app.use(bodyParser.json());
 
     logger.inProd('Mounting API...');
-    app.get('/api', createApiVersioningRouter(path.join(__dirname, 'api')));
+    app.use('/api', createApiVersioningRouter(path.join(__dirname, 'api')));
 
     logger.inProd('Mounting routes...');
     listFiles(routesPath)
@@ -54,7 +66,7 @@ export function createServer(bind) {
           ? tempRoute.URL
           : rpath.substr(0, rpath.lastIndexOf('.')) || rpath;
         logger.inProd(
-          chalk`Mounting {white ${rpath}} on {white ${mountpoint}}`
+          chalk `Mounting {white ${rpath}} on {white ${mountpoint}}`
         );
         app.use(mountpoint, tempRoute);
       });
@@ -67,6 +79,17 @@ export function createServer(bind) {
       res.json({ data: { healthy: true } })
     );
 
+    app.get('/auth_status', (req, res) =>
+      res.json({
+        'auth_status': req.isAuthenticated(),
+        'user': req.user,
+      })
+    );
+    app.get('/login_failure', (req, res) => {
+      // TODO: write proper implementation
+      res.write('Sorry we couldn\'t find your user.\n');
+      res.end();
+    });
     const server = app.listen(bind, () => {
       console.log(`✅  Application bound and running`);
       resolve(server);
